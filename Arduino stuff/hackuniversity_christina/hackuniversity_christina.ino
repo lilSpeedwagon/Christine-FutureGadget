3d40b76c5e5632c35052f4fed422acd78d2f3a0b
FF220v <San987873@yandex.ru>
2018-04-14 00:28:15 +0300

    Съездили за доской, поели блинчиков

--- Arduino stuff/hackuniversity_christina/hackuniversity_christina.ino
+++ Arduino stuff/hackuniversity_christina/hackuniversity_christina.ino
@@ -1,3 +1,18 @@
+#include <Servo.h>
+
+#define STICKX A0
+#define STICKY A1
+#define STEPL A2
+#define DIRL A3
+#define STEPL A4
+#define DIRL A5
+
+class StepperMotor{
+  
+  
+  };
+
+
 void setup() {
 
 }
